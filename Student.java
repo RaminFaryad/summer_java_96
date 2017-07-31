ebdc07214924d1843cd20022cd3140a4a22cc75d
RaminFaryad <faryad_raminharis@yahoo.com>
2017-07-31 21:28:52 +0430

    student_class

---
+++ Student.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Student extends Person {
+	public Student(String name ,String house, String bloodStatus , String school, Date birthday){
+		super(name ,house,bloodStatus ,school,birthday);	
+	}
+}
